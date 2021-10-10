6144a205c3dc63e775bc9496690e40e13ba90202
Afanasiev Dmitriy <59284294+d-afanasiev@users.noreply.github.com>
2021-10-10 18:58:56 +0300

    change redux v.3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import "./App.css";
 import PropTypes from "prop-types";
 // import { connect } from "react-redux";
-import { useSelector } from "react-redux";
 import ContactForm from "./components/ContactForm";
 import Filter from "./components/Filter";
 import ContactList from "./components/ContactList";
 
 export default function App() {
-  const contacts = useSelector((state) => state.contacts.items);
-  const filter = useSelector((state) => state.contacts.filter);
-
-  const getVisibleList = () => {
-    const normalizeFilter = filter.toLowerCase();
-
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(normalizeFilter)
-    );
-  };
-
   return (
     <div className="App">
       <h1 className="titlePhonebook">Phonebook</h1>
@@ -25,7 +13,7 @@ export default function App() {
 
       <h1 className="titleContacts">Contacts</h1>
       <Filter />
-      <ContactList getVisibleContacts={getVisibleList} />
+      <ContactList />
     </div>
   );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,27 @@
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
 // import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import * as actions from "../../redux/actions";
 import css from "./ContactList.module.css";
+import { getContacts, getFilter } from "../../redux/selector";
 
-export default function ContactList({ getVisibleContacts, filter }) {
+export default function ContactList() {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const visibleList = () => {
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
   const dispatch = useDispatch();
   const deleteContact = (value) => dispatch(actions.deleteContacts(value));
-  const visibleList = getVisibleContacts(filter);
   return (
     <ul className={css.listContacts}>
-      {visibleList.map((contact) => (
+      {visibleList().map((contact) => (
         <li key={contact.id} className={css.item}>
           {contact.name}: {contact.number}
           <button
